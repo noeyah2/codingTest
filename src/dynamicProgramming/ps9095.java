e0e977898e51135887bc896f65faccb268f9d26a
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-09-19 19:49:29 +0900

    230919

--- src/dynamicProgramming/ps9095.java
+++ src/dynamicProgramming/ps9095.java
@@ -4,32 +4,23 @@ import java.io.*;
 import java.util.*;
 
 public class ps9095 {
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int[] arr = new int[T];
 
-        for (int i=0 ; i<arr.length;i++) {
+        int[] dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int j=4 ; j<=10 ; j++) {
+            dp[j] = dp[j-3] + dp[j-2] + dp[j-1];
+        }
+
+        for (int i=0 ; i<T ; i++) {
             int N = Integer.parseInt(br.readLine());
-            dp = new Integer[N+1];
-            dp[0] = 0;
-            System.out.println(recur(N));
-        }
 
-    }
-    static int recur(int N){
-        if (N == 1) {
-            dp[N] = 1;
-        } else if (N == 2) {
-            dp[N] = 2;
-        } else if (N == 3) {
-            dp[N] = 4;
-        } else {
-            dp[N] = recur(N-1) + recur(N-2) + recur(N-3);
+            System.out.println(dp[N]);
         }
-        return recur(N);
     }
 }
